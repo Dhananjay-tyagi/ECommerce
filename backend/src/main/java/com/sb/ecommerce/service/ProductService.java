bb3d446ca75be54182f04d089e0dd703edf00769
Dhananjay-tyagi <dhananjaytyagi321@gmail.com>
2026-07-28 09:08:54 +0530

    Pushing before adding payment gateway

---
+++ backend/src/main/java/com/sb/ecommerce/service/ProductService.java
@@ -0,0 +1,48 @@
+package com.sb.ecommerce.service;
+
+import com.sb.ecommerce.model.Product;
+import com.sb.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository repository;
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+
+    }
+
+    public Product getProductById(int id) {
+        return repository.findById(id).get();
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repository.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return repository.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        repository.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return repository.searchProducts(keyword);
+    }
+}
